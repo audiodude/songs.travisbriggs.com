3dd0ef83106cc38154ff8633c65aadd2cc22d0c6
Travis Briggs <audiodude@gmail.com>
2018-05-28 14:24:36 -0700

    Show "related" songs based on tags on the song show page.

--- sitebuilder.py
+++ sitebuilder.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from datetime import datetime
 import sys
 import os
@@ -14,20 +15,36 @@ app.config.update(
 songs = FlatPages(app)
 freezer = Freezer(app)
 
+def _annotate(song):
+  if hasattr(song, 'slug'):
+    return
+  slug = os.path.basename(song.path)
+  song.src = '/static/' + slug + '.mp3'
+  song.slug = slug
+  song.dt = datetime.strptime(song.meta['date'], '%Y/%m/%d')
+
 @app.route('/')
 def index():
   for song in songs:
-    slug = os.path.basename(song.path)
-    song.src = '/static/' + slug + '.mp3'
-    song.slug = slug
-    song.dt = datetime.strptime(song.meta['date'], '%Y/%m/%d')
+    _annotate(song)
 
   sorted_songs = sorted(list(songs), key=lambda song: song.dt, reverse=True)
   return render_template('index.html', songs=sorted_songs)
 
 @app.route('/<path:path>/')
 def song(path):
+  related = defaultdict(list)
+
   song = songs.get_or_404(path)
+  _annotate(song)
+
+  for tag in song.meta['tags']:
+    for s in songs:
+      if tag in s.meta['tags']:
+        _annotate(s)
+        if s.slug != song.slug:
+          related[tag].append(s)
+  song.related = related
   song.src = '/static/' + os.path.basename(path) + '.mp3'
   return render_template('song.html', song=song, title=song.meta['title'])
 
