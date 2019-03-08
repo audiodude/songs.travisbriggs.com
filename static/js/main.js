e52ba13a91e121b51fedfc4d7c696476f9e92b4c
Travis Briggs <audiodude@gmail.com>
2019-03-07 22:19:03 -0800

    Re-write player with progress bar and current time

--- static/js/main.js
+++ static/js/main.js
@@ -1,41 +1,80 @@
 $(function() {
   var globalPause;
 
-  function play(playButton, player) {
+  function play(playButton, audioPlayer) {
     $(playButton).find('.fa-play').hide();
     $(playButton).find('.fa-pause').show();
-    player.play();
-    $.data(player, 'playing', true);
+    audioPlayer.play();
+    $.data(audioPlayer, 'playing', true);
   }
 
-  function pause(playButton, player) {
+  function pause(playButton, audioPlayer) {
     $(playButton).find('.fa-pause').hide();
     $(playButton).find('.fa-play').show();
-    player.pause();
-    $.data(player, 'playing', false);
+    audioPlayer.pause();
+    $.data(audioPlayer, 'playing', false);
+  }
+
+  function updateDuration(player, duration) {
+    $(player).find('.duration').text(formatTime(duration));
+  }
+
+  function formatTime(seconds) {
+    var minutes = Math.floor(seconds / 60);
+    var secsRem = Math.round(seconds % 60);
+    if (secsRem < 10) {
+      secsRem = '0' + secsRem;
+    }
+    return minutes + ':' + secsRem;
   }
 
   $('.player').each(function(i, player) {
     var song_id = $(player).attr('data-song-id');
+    var audioPlayer = $('#player-' + song_id).get(0);
 
-    player.addEventListener('ended', function() {
+    audioPlayer.addEventListener('ended', function() {
       pause($(player).find('.play-button').get(0), player);
       player.currentTime = 0;
     }, true);
 
+    audioPlayer.addEventListener('timeupdate', function() {
+      var percent = audioPlayer.currentTime * 100 / audioPlayer.duration;
+      $(player).find('.progress').css({'width': percent + '%'});
+      $(player).find('.current-time').text(formatTime(audioPlayer.currentTime));
+    });
+
+    audioPlayer.addEventListener('loadedmetadata', function() {
+      updateDuration(player, audioPlayer.duration);
+    });
+
+    audioPlayer.addEventListener('play', function() {
+      updateDuration(player, audioPlayer.duration);
+    });
+
     $(player).find('.play-button').click(function() {
-      var player = $('#player-' + song_id).get(0)
-      if ($.data(player, 'playing')) {
-        pause(this, player);
+      if ($.data(audioPlayer, 'playing')) {
+        pause(this, audioPlayer);
       }
       else {
         if (globalPause) {
           globalPause();
           globalPause = null;
         }
-        play(this, player);
-        globalPause = pause.bind(null, this, player);
+        play(this, audioPlayer);
+        globalPause = pause.bind(null, this, audioPlayer);
       }
     });
   });
+
+  window.setTimeout(function() {
+    $('.duration').each(function(i, duration) {
+      if ($(duration).text() == '0:00') {
+        var player = $(duration).closest('.player');
+        var song_id = $(player).attr('data-song-id');
+        var audioPlayer = $('#player-' + song_id).get(0);
+
+        $(duration).text(formatTime(audioPlayer.duration));
+      }
+    });
+  }, 4000);
 });
