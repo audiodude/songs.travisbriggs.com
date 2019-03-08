3478026c457b17b78b586faa68d4d4bc4fe9f982
Travis Briggs <audiodude@gmail.com>
2019-03-07 20:15:27 -0800

    Finished homepage with non-working scrubber

--- sitebuilder.py
+++ sitebuilder.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from datetime import datetime
+import random
 import sys
 import os
 
@@ -15,20 +16,39 @@ app.config.update(
 songs = FlatPages(app)
 freezer = Freezer(app)
 
-def _annotate(song):
+song_colors = [
+  '#FD7632',
+  '#477C90',
+  '#4B0082',
+  '#CD4640',
+  '#85D817',
+  '#DDB8C7',
+  '#2AC8C6',
+]
+
+def _annotate(song, i):
   if hasattr(song, 'slug'):
     return
   slug = os.path.basename(song.path)
   song.src = '/static/' + slug + '.mp3'
   song.slug = slug
   song.dt = datetime.strptime(song.meta['date'], '%Y/%m/%d')
+  _add_color(song, i)
+
+def _add_color(song, i):
+  song.color = song_colors[i % len(song_colors)]
 
 @app.route('/')
 def index():
-  for song in songs:
-    _annotate(song)
+  for i, song in enumerate(songs):
+    _annotate(song, i)
 
   sorted_songs = sorted(list(songs), key=lambda song: song.dt, reverse=True)
+
+  # Re-add the colors once the songs are sorted.
+  for i, song in enumerate(sorted_songs):
+    _add_color(song, i)
+
   return render_template('index.html', songs=sorted_songs)
 
 @app.route('/<path:path>/')
@@ -36,12 +56,12 @@ def song(path):
   related = defaultdict(list)
 
   song = songs.get_or_404(path)
-  _annotate(song)
+  _annotate(song, random.randrange(0, len(song_colors)))
 
   for tag in song.meta['tags']:
-    for s in songs:
+    for i, s in enumerate(songs):
       if tag in s.meta['tags']:
-        _annotate(s)
+        _annotate(s, i)
         if s.slug != song.slug:
           related[tag].append(s)
   song.related = related
